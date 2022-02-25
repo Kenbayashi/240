ab3171442776e47d70f876bf46db74713563a8d3
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-02-25 12:54:19 +0900

    a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -1,3 +1,9 @@
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        a: i32,
+        b: i32,
+    }
+
+    let ans = b - a == 1 || b - a == 9;
+    println!("{}", if ans {"Yes"} else {"No"});
 }
