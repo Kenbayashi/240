ff12ecbcc62eda79878e0f6c37365dc58e3bfe65
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-02-25 13:02:28 +0900

    b

--- src/bin/b.rs
+++ src/bin/b.rs
@@ -1,3 +1,15 @@
+use std::collections::HashMap;
+
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        n: usize,
+        ts: [i32; n],
+    }
+
+    let mut map = HashMap::<i32, i32>::new();
+
+    ts.into_iter()
+      .for_each(|t| *map.entry(t).or_default() += 1);
+
+    println!("{}", map.len());
 }
