cbe0c5d8ff3c6b1fe942819ed8910bc0822cfde0
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-02-25 13:36:03 +0900

    c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,3 +1,24 @@
 fn main() {
-    unimplemented!();
+    proconio::input! {
+        n: usize,
+        x: i32,
+        lens: [(i32, i32); n],
+    }
+
+    let mut acc = false;
+
+    for bits in 0..(1 << n) {
+        let sub_list = (0..n).into_iter()
+                             .map(|x| bits & (1 << x) != 0)
+                             .collect::<Vec<bool>>();
+
+        let ans = lens.iter()
+                      .enumerate()
+                      .map(|(index, &(a, b))| if sub_list[index] {a} else {b})
+                      .sum::<i32>();
+
+        acc = acc || ans == x;
+    }
+
+    println!("{}", if acc {"Yes"} else {"No"});
 }
